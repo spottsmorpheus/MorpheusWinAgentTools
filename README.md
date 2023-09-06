a07a5b6a713d26042406db9cc12e2e9c39ed2598
spottsmorpheus <spotts@morpheusdata.com>
2023-09-06 09:32:38 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,106 @@
 # MorpheusWinAgentTools
-Powershell tool for viewing and configuring Morpheus Windows agent
+
+## Powershell tool for viewing and configuring Morpheus Windows agent
+
+This Powershell script contains a number of functions to view and edit the Morpheus Windows Agent configuratiion file and also view the current connected status of the Agent service
+
+To load the script Dot Source the MorpheusAgentFunctions.ps1 file into a powershell session
+
+```
+PS> . .\MorpheusAgentFunctions.ps1
+```
+
+**NOTE** to run these Powershell Scripts the account must be and Administrator and the session elevated. The scriopt will warn if there are 
+insuccifient access rights.
+
+Functions have some help available which can be access via the Powershell Get-Help \<Function-Name\> command
+
+## Loading Directly from GitHub URL
+
+It is possible to load these Functions directly from GitHub if your Endpoint has an Internet connection. Use the following  Powershell to download and Install a Dynamic Module directly from a GitHub Url
+
+```
+$Uri = "https:://GitRaw"
+# Load Powershell code from GitHub Uri and invoke as a temporary Module
+$Response = Invoke-WebRequest -Uri $Uri -UseBasicParsing
+if ($Response.StatusCode -eq 200) {
+    New-Module -Name "MorpheusAgentFunctions" -ScriptBlock ([ScriptBlock]::Create($Response.Content))
+}
+```
+
+## About the Functions
+
+It is possible to run these Powershell script from a Morpheus Task. You can use a Remote task (winRm) or even the Morpheus Agent itself. When changes are made to the agent config using the Morpheus Agent, a delayed restart is used to ensure the task completes and returns an acknowledgement to Morpheus, preventing possible issues with the task being re-queued by RabbitMq
+
+### Get-MorpheusAgentConfig
+
+Returns the current XML Documument containing the Agent configuration
+
+### Set-MorpheusAgentConfig
+
+Used to update the Agent XML Config. 
+
+### Get-MorpheusAgentSocketStatus
+
+```
+NAME
+    Get-MorpheusAgentSocketStatus
+
+SYNOPSIS
+    Returns the Morpheus Windows Agent Service Status and associated TCP Socket details
+
+
+SYNTAX
+    Get-MorpheusAgentSocketStatus [-AsJson] [<CommonParameters>]
+```
+
+### Get-MorpheusAgentConfig
+
+```
+NAME
+    Get-MorpheusAgentConfig
+
+SYNOPSIS
+    Returns the current contents of the Morpheus Windows Agent config file
+
+
+SYNTAX
+    Get-MorpheusAgentConfig [<CommonParameters>]
+```
+
+### Set-MorpheusAgentConfig
+
+```
+
+NAME
+    Set-MorpheusAgentConfig
+
+SYNOPSIS
+    Used to perform safe updates to the Morpheus Agent Configuration file
+
+
+SYNTAX
+    Set-MorpheusAgentConfig [[-LogLevel] <Int32>] [[-ApplianceUrl] <String>] [[-ApiKey] <String>] [-RestartAgent]
+    [[-SystemNetProxyXml] <String>] [<CommonParameters>]
+
+```
+
+### Examples
+
+Set the Morpheus Agent log level to Informational level (1) restarting the agent after a 60 second delay
+
+```
+Set-MorpheusAgentConfig -LogLevel 1 -RestartAgent
+```
+
+Set the Morpheus Agent log level to Debug level (0) and set the ApiKey. The config file will be updated but changes will not take effect until the agent is restarted.
+
+```
+Set-MorpheusAgentConfig -LogLevel 0 -ApiKey "0538e2b0-d85f-4377-9f56-07df0ab04ea2"
+```
+
+To simply restart the agent after a 60 second delay
+
+```
+Set-MorpheusAgentConfig  -RestartAgent
+```
---
+++ src/MorpheusAgentFunctions.ps1
@@ -0,0 +1,336 @@
+
+Function Set-MorpheusAgentConfig {
+    <#
+    .SYNOPSIS
+        Used to perform safe updates to the Morpheus Agent Configuration file
+        
+    .PARAMETER LogLevel
+        Sets the LogLevel 0 = debug; 1 = info; 2 = warn; 3 = error; 4 = off
+        Default value is 1
+
+    .PARAMETER ApplianceUrl
+        Optionally sets the appliance Host. There is no Default value.
+        Only specify if you need to change the Host name
+
+        Format is "https://ApplianceNameOrIp/"
+
+    .PARAMETER ApiKey
+        Optionally sets the Instance ApiKey. There is no Default value.
+        Only specify if you need to change the ApiKey
+
+        The ApiKey MUST match exactly the ApiKey on the Compute Server details page
+    
+    .PARAMETER SystemNetProxyXml
+        Use this parameter to safely insert a well formed XML fragment for the agent Proxy configuration
+        
+        For example the Xml should be formed as so
+
+        <system.net>
+            <defaultProxy>
+                <proxy usesystemdefault="False" proxyaddress="http://10.32.23.194:3128" bypassonlocal="False" />
+            </defaultProxy>
+        </system.net>
+
+        See Microsoft documentation for full details
+
+    .PARAMETER RestartAgent
+        Specify this parameter to schedule a delayed restart (60 seconds) of the Morpheus Windows Agent Service
+
+    .OUTPUTS
+        if successful the updated contents of the MorpheusAgent.exe.config file are returned and an XML String
+
+    #> 
+    [CmdletBinding()]
+    param (
+        [ValidateRange(0,4)]
+        [Int32]$LogLevel=1,
+        [String]$ApplianceUrl=$null,
+        [String]$ApiKey=$null,
+        [Switch]$RestartAgent,
+        [String]$SystemNetProxyXml=""
+    )
+    
+    if (IsElevated) {
+        $Agent = Get-CIMInstance -Class win32_Service -Filter "Name like 'Morpheus Windows Agent'"
+        if ($Agent) {
+            $AgentFolder = Split-Path -Path ($Agent.PathName -replace '"','') -Parent
+            $ConfigFile = Join-Path -Path $AgentFolder -ChildPath "MorpheusAgent.exe.config"
+            if (Test-Path -Path $ConfigFile) {
+                # Config file exists - load the file as XML - Fail and exit if not valid
+                try {
+                    [Xml]$Config = Get-Content -Path $ConfigFile -Raw
+                }
+                Catch {
+                    Write-Warning "Failed to Parse XML Agent Config file"
+                    $Config = $null
+                }
+                if ($Config) {
+                    #Navigate the XML - run through the app settings modifying the attributes as required
+                    foreach ($node in $Config.SelectNodes("/configuration/appSettings/add")) {
+                        if (($LogLevel -ge 0 -AND $LogLevel -lt 5) -AND $node.GetAttribute("key") -eq "LogLevel") {$node.SetAttribute("value",$LogLevel.toString())}
+                        if ($ApplianceUrl -AND $node.GetAttribute("key") -eq "Host") {$node.SetAttribute("value",$ApplianceUrl)}
+                        if ($ApiKey -AND $node.GetAttribute("key") -eq "ApiKey") {$node.SetAttribute("value",$ApiKey)}
+                    }
+                    # Optionally Configure <system.Net> by adding XML $SystemNetXML
+
+                    if ($SystemNetProxyXml) {
+                        try {
+                            [Xml]$systemNet= $SystemNetProxyXml
+                            Write-Host "Paramater Option specifies XML for <system.net> element" -ForegroundColor Green
+                            Write-Host $(XmlPrettyPrint -Xml $SystemNetProxyXml) -ForegroundColor Cyan
+                            # Import the new node ready to use in the Config XML document
+                            # <system.net> Node for import
+                            $newSystemNetNode = $Config.ImportNode($systemNet.SelectSingleNode("/system.net"),$True)
+                            # <defaultProxy> Node for import
+                            $newProxyNode = $newSystemNetNode.SelectSingleNode("/defaultProxy")
+
+                        }
+                        catch {
+                            Write-Error "SystemNetProxyXml is badly formed - check your XML"
+                            return
+                        }
+                        $systemNetNode = $Config.SelectSingleNode("/configuration/system.net")
+                        if ($systemNetNode) {
+                            #<system.net> element exists replace defaultProxy element with ours
+                            $oldProxyNode = $Config.SelectSingleNode("/configuration/system.net/defaultProxy")                            
+                            if ($oldProxyNode) {
+                                #replace XML
+                                Write-Warning "<defaultProxy> Node exists in current config - replacing <defaultProxy> element"
+                                $ret=$Config.SelectNodes("/configuration/system.net").ReplaceChild($newProxyNode,$oldProxyNode)
+                            } else {
+                                #Add
+                                Write-Host "Inserting <defaultProxy> Node into current config" -ForegroundColor Green
+                                $ret=$Config.SelectNodes("/configuration/system.net").AppendChild($newProxyNode)
+                            }
+                        } else {
+                            Write-Host "<system.net> Node does not exist in current config. Inserting XML" -foregroundcolor green
+                            # Insert new system.net element
+                            $ret=$Config.SelectSingleNode("/configuration").AppendChild($newSystemNetNode)
+                        }
+                    }
+
+                    Write-Host "Saving new config "
+                    $Config.Save($ConfigFile)
+                    # Restart Service - use Delay-AgentRestart to detach a process to do the restart otherwise it kills this job
+                    if ($RestartAgent) {
+                        $RestartPid = Delay-AgentRestart -Delay 60
+                        Write-Host "Delaying Agent Service Restart - detaching process $($RestartPid)"
+                    }
+                    else {
+                        Write-Warning "Agent Service must be restarted to use new configuration"
+                    }
+                    Write-Host "Returning Updated Agent Config ..."
+                    # Return the updated Config File (as a String)
+                    return (Get-MorpheusAgentConfig)
+                }
+            }
+            else {
+                Write-Warning "Agent Config file $($ConfigFile) Not Found"
+            }
+        }
+        else {
+            Write-Warning "Morpheus Windows Agent Not Found"
+        }
+    }
+    else {
+        Write-Warning "This function must have Administrator privilege"
+    }
+} 
+
+Function Get-MorpheusAgentConfig {
+    <#
+    .SYNOPSIS
+        Returns the current contents of the Morpheus Windows Agent config file
+
+    .OUTPUTS
+        if successful the contents of the MorpheusAgent.exe.config file are returned as XML String
+
+    #>     
+    if (IsElevated) {
+        $Agent = Get-CIMInstance -Class win32_Service -Filter "Name like 'Morpheus Windows Agent'"
+        if ($Agent) {
+            $AgentFolder = Split-Path -Path ($Agent.PathName -replace '"','') -Parent
+            $ConfigFile = Join-Path -Path $AgentFolder -ChildPath "MorpheusAgent.exe.config"
+            if (Test-Path -Path $ConfigFile) {
+                # Config file exists
+                $CF = Get-Content -Path $ConfigFile -Raw
+                return $CF
+            }
+            else {
+                Write-Warning "Agent Config file $($ConfigFile) Not Found"
+            }
+        }
+        else {
+            Write-Warning "Morpheus Windows Agent Not Found"
+        }
+    }
+    else {
+        Write-Warning "This function must have Administrator privilege"
+    }
+}
+
+
+Function IsElevated {
+    <#
+    .SYNOPSIS
+        Helper function.
+        Determines if the current user has Administrator Privilege
+
+    .OUTPUTS
+        $true - Current user has Administrator role
+        $false = Curent User does not have Administrator Role
+
+    #>     
+    $userIdentity =  [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $userPrincipal = [System.Security.Principal.WindowsPrincipal]$UserIdentity
+    $adminElevated=$UserPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    return $adminElevated
+}
+
+Function Get-MorpheusAgentSocketStatus {
+    <#
+    .SYNOPSIS
+        Returns the Morpheus Windows Agent Service Status and associated TCP Socket details 
+
+    .PARAMETER AsJson
+        Specify this parameter to return output as a json string
+
+    .OUTPUTS
+        PSCustomObject, or optionally json string containing the Agent and Socket status 
+
+    #> 
+    [CmdletBinding()]
+    param (
+        [Switch]$AsJson
+    )
+
+    $Status = [PSCustomObject]@{
+        adminAccess=IsElevated;
+        machineName=[Environment]::MachineName;
+        agentStatus="";
+        agentState=""
+        agentPid=$null;
+        agentSockets=$null
+    }
+
+    if (IsElevated) {
+        $Agent = Get-CIMInstance -Class win32_Service -Filter "Name like 'Morpheus Windows Agent'"
+        if ($Agent) {
+            $Status.agentStatus = $Agent.Status
+            $Status.agentState = $Agent.State
+            # We have an agent - Does it have a Pid
+            if ($Agent.ProcessId -Ne 0) {
+                $Status.agentPid = $Agent.ProcessId
+                try {
+                    $Sockets = @(Get-NetTCPConnection -RemotePort 443 -OwningProcess $Agent.ProcessId)
+                }
+                catch {
+                    $Sockets = @()
+                    Write-Warning "Agent ProcessId Owns no Sockets on port 443"
+                }
+                $Status.agentSockets = foreach ($Socket in $Sockets) {
+                    [PSCustomObject]@{
+                        state = $Socket.State.ToString();
+                        creationTime = $Socket.CreationTime.ToString("s");
+                        localAddress = $Socket.LocalAddress;
+                        localPort = $Socket.LocalPort;
+                        remoteAddress = $Socket.RemoteAddress;
+                        remotePort = $Socket.RemotePort
+                    } 
+                }
+            }
+            else {
+                Write-Warning "Morpheus Windows Agent Installed but Not Running on  $($Status.machineName)"
+                $Status.agentStatus="NotRunning"                
+            }
+        }
+        else {
+            Write-Warning "Morpheus Windows Agent Not Installed on  $($Status.machineName)"
+            $Status.agentStatus="NoAgent"
+        }         
+    }
+    else {
+        Write-Warning "You need to be an Administrator to run this Function"
+    }
+    if ($AsJson) {
+        return $Status | ConvertTo-Json -Depth 5
+    }
+    else {
+        return $Status
+    }
+}
+
+Function Delay-AgentRestart {
+    <#
+    .SYNOPSIS
+        Helper Tool used to detach a process to delay a restart of the Morpheus Windows Agent service.
+        This way you can restart the Agent from a command bus session without affecting the running job 
+
+    .PARAMETER Delay
+        Number of seconds to Wait before the Agent is restarted. Default is 30
+
+    .OUTPUTS
+        Returns the Process Id of the detached process responsible for restarting the Morpheus Windows Agent service.
+        Returns 0 if the Detached process failed to start successfully
+
+    #>     
+    [CmdletBinding()]
+    param (
+        [Int32]$Delay=30
+    )
+
+        $ArgString = " -noprofile -command ""& {start-sleep -seconds $($Delay); Restart-Service 'Morpheus Windows Agent' }"" "
+        $RestartProcess = Start-Process -FilePath "powershell.exe" -ArgumentList $ArgString -Verb "RunAs" -WindowStyle "Hidden" -Passthru
+        if ($RestartProcess) {
+            return $RestartProcess.Id
+        }
+        else {
+            return 0
+        }
+} 
+
+Function XmlPrettyPrint {
+    <#
+    .SYNOPSIS
+        Helper Tool used to Pretty format an XML String
+
+    .PARAMETER Xml
+        Number of seconds to Wait before the Agent is restarted. Default is 30
+
+    .OUTPUTS
+        Returns a Pretty formated XML String
+
+    #>     
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Xml
+    )
+
+    if ($Xml) {
+        $stringReader = New-Object System.IO.StringReader($Xml)
+        $settings = New-Object System.Xml.XmlReaderSettings
+        $settings.CloseInput = $true
+        $settings.IgnoreWhitespace = $true
+        $reader = [System.Xml.XmlReader]::Create($stringReader, $settings)
+        $stringWriter = New-Object System.IO.StringWriter
+        $settings = New-Object System.Xml.XmlWriterSettings
+        $settings.CloseOutput = $true
+        $settings.Indent = $true
+        $writer = [System.Xml.XmlWriter]::Create($stringWriter, $settings)
+       
+        while (!$reader.EOF) {
+            $writer.WriteNode($reader, $false)
+        }
+        $writer.Flush()
+       
+        $result = $stringWriter.ToString()
+        $reader.Close()
+        $writer.Close()
+        $result
+    } else {
+        Write-Warning "XML String is Empty"
+        return ""
+    }
+
+}
